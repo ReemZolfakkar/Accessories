87398909c54bbdaca31214c04e35808c286c274b
Doaa Essam <due_essam_2015@yahoo.com>
2022-02-19 14:50:50 +0200

    big screens navbar.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import "./Styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/Navbar.css";
+import "./Styles/App.css";
 import Navbar from "./Components/Navbar";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
@@ -12,23 +13,26 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Switch>
-        <Route path="/ShopAll">
-          <ShopAll />
-        </Route>
-        <Route path="/OurStory">
-          <OurStory />
-        </Route>
-        <Route path="/OurCraft">
-          <OurCraft />
-        </Route>
-        <Route path="/Contact">
-          <Contact />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <div className="App">
+        <Switch>
+          <Route path="/ShopAll">
+            <ShopAll />
+          </Route>
+          <Route path="/OurStory">
+            <OurStory />
+          </Route>
+          <Route path="/OurCraft">
+            <OurCraft />
+          </Route>
+          <Route path="/Contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+     
+      </div>
     </BrowserRouter>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,53 +1,112 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaPinterestP,
+  FaTwitter,
+  FaUserCircle,
+} from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 
 function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="collapse navbar-collapse" id="navbar">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/"}>Home</Link>
-            
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/ShopAll">
-              Shop All
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/OurStory">
-              Our Story
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/OurCraft">
-              Our Craft
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/Contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
-        {/* <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-      */}
+  const [tabs] = useState([
+    { url: "", title: "Home" },
+    { url: "ShopAll", title: "Shop All" },
+    { url: "OurStory", title: "Our Story" },
+    { url: "OurCraft", title: "Our Craft" },
+    { url: "Contact", title: "Contact" },
+  ]);
+  const [socialIcons] = useState([
+    <FaFacebookF />,
+    <FaInstagram />,
+    <FaTwitter />,
+    <FaPinterestP />,
+    <div className="nav-top--login-cart">
+      <div>
+        <span className="user-icon">
+          <FaUserCircle />
+        </span>
+        <span className="p-s--9">Log In</span>
       </div>
-    </nav>
+      <span className="cart">Cart(0)</span>
+    </div>,
+    ,
+  ]);
+  const showHideNav=()=>{
+    var prevY = window.pageYOffset;
+    window.onscroll = function() {
+      var currentY = window.pageYOffset;
+      //show on scroll up 
+      if ( prevY >currentY )  {
+        document.getElementById("nav-section").style.top = "0px";
+      }
+      //hide on scroll down 
+      else{
+        document.getElementById("nav-section").style.top = "-1000px";
+      }
+      prevY=currentY;
+    }
+  }
+  useEffect(()=>{
+    showHideNav();
+  },[])
+  const [activeTab, setActive] = useState("");
+  return (
+    <div id="nav-section">
+      <div className="row nav-top">
+        <div className="col-lg-4 col-12">
+          <form className="search-box">
+            <FiSearch />
+            <input className="search" type="search" placeholder="Search..." />
+          </form>
+        </div>
+        <div className="col-lg-4 col-2 nav-brand">adalene.</div>
+        <div className="col-lg-4 col-10">
+          <ul className="navbar-nav--head align-items-center">
+            {socialIcons.map((element) => {
+              return (
+                <li key={Math.random()*1000} className="nav-item--top">
+                  <a className="nav-link--top" href="#">
+                    {element}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+     
+      </div>
+      <nav className="navbar navbar-expand-sm">
+        <div className="collapse navbar-collapse" id="navbar">
+          <ul className="navbar-nav">
+            {tabs.map((tab) => {
+              return (
+                <li
+                  id={tab.url}
+                  key={Math.random() * 1000}
+                  className="nav-item"
+                >
+                  <Link
+                    className={`${
+                      activeTab == tab.url
+                        ? "nav-link nav-link--active"
+                        : "nav-link"
+                    }`}
+                    onClick={() => {
+                      setActive(tab.url);
+                    }}
+                    to={`/${tab.url}`}
+                  >
+                    {tab.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 
