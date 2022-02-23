b821604fbd5b81947be11e52228d1956d9d6d3aa
Doaa Essam <due_essam_2015@yahoo.com>
2022-02-23 14:48:07 +0200

    payment form ui

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 import { RiVisaLine } from "react-icons/ri";
-import Payment_Validate from './../../Functions/Payment_Validate';
+import Payment_Validate from "./../../Functions/Payment_Validate";
 function Checkout() {
   const [products, setProducts] = useState([
     { name: "product1", price: "20$" },
@@ -13,17 +13,17 @@ function Checkout() {
     Expires: "",
     CVC: "",
   });
-  const [formsErrors,setErrors]=useState({});
+  const [formsErrors, setErrors] = useState({});
   const handleInput = (e) => {
-    let key=e.target.name;
-    let value=e.target.value;
-    setVisaData({...visaData,[key]:value});
+    let key = e.target.name;
+    let value = e.target.value;
+    setVisaData({ ...visaData, [key]: value });
     setErrors({});
   };
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     setErrors(Payment_Validate(visaData));
-  }
+  };
   return (
     <div className="row Checkout">
       <div className="col-4 Chechout__summarize">
@@ -37,52 +37,63 @@ function Checkout() {
         })} */}
       </div>
       <div className="col-4 Chechout__form">
-        <div className="Chechout__form__visa">
-          <RiVisaLine />
+        <div>
+          <div className="Chechout__form__visa">
+            <RiVisaLine />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="row mx-0">
+              <div className="col-12 Chechout__form__items">
+                <label className="Chechout__form__label">Card Number</label>
+                <input
+                  type="number"
+                  className="Chechout__form__inputs"
+                  name="CardNumber"
+                  onChange={handleInput}
+                ></input>
+                <p className="Chechout__form__error">
+                  {formsErrors.CardNumber}
+                </p>
+              </div>
+              <div className="col-12 Chechout__form__items">
+                <label className="Chechout__form__label">Card Holder</label>
+                <input
+                  type="text"
+                  className="Chechout__form__inputs"
+                  name="CardHolder"
+                  onChange={handleInput}
+                ></input>
+                <p className="Chechout__form__error">
+                  {formsErrors.CardHolder}
+                </p>
+              </div>
+
+              <div className="col-6 Chechout__form__items">
+                <label className="Chechout__form__label">Expires</label>
+                <input
+                  type="text"
+                  className="Chechout__form__inputs"
+                  name="Expires"
+                  onChange={handleInput}
+                ></input>
+                <p className="Chechout__form__error">{formsErrors.Expires}</p>
+              </div>
+              <div className="col-6 Chechout__form__items">
+                <label className="Chechout__form__label">CVC</label>
+                <input
+                  type="number"
+                  className="Chechout__form__inputs"
+                  name="CVC"
+                  onChange={handleInput}
+                ></input>
+                <p className="Chechout__form__error">{formsErrors.CVC}</p>
+              </div>
+            </div>
+            <div className="text-center">
+              <button className="Chechout__form__button" type="submit">Checkout</button>
+            </div>
+          </form>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="Chechout__form__items">
-            <label className="Chechout__form__label">Card Number</label>
-            <input
-              type="number"
-              className="Chechout__form__inputs"
-              name="CardNumber"
-              onChange={handleInput}
-            ></input>
-            <p className="Chechout__form__error">{formsErrors.CardNumber}</p>
-          </div>
-          <div className="Chechout__form__items">
-            <label className="Chechout__form__label">Card Holder</label>
-            <input
-              type="text"
-              className="Chechout__form__inputs"
-              name="CardHolder"
-              onChange={handleInput}
-            ></input>
-            <p className="Chechout__form__error">{formsErrors.CardHolder}</p>
-          </div>
-          <div className="Chechout__form__items">
-            <label className="Chechout__form__label">Expires</label>
-            <input
-              type="text"
-              className="Chechout__form__inputs"
-              name="Expires"
-              onChange={handleInput}
-            ></input>
-            <p className="Chechout__form__error">{formsErrors.Expires}</p>
-          </div>
-          <div className="Chechout__form__items">
-            <label className="Chechout__form__label">CVC</label>
-            <input
-              type="number"
-              className="Chechout__form__inputs"
-              name="CVC"
-              onChange={handleInput}
-            ></input>
-            <p className="Chechout__form__error">{formsErrors.CVC}</p>
-          </div>
-          <button type="submit" >submit</button>
-         </form>
       </div>
     </div>
   );
